a1a4ef111d548e2d77371e66d6202607eb18e2d7
moxunjingmu <1786471868@qq.com>
2022-04-12 00:04:51 +0800

    feat：创建graph图

--- example/main.js
+++ example/main.js
@@ -1,4 +1,4 @@
-import { foo } from "./foo";
+import { foo } from "./foo.js"; //不加.js 获取文件时会报错
 
 foo()
 console.log("main");
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import fs from 'fs'
+import path from 'path'
 import parser from '@babel/parser'
 import traverse from '@babel/traverse'
 
-function createAsset() {
+function createAsset(filePath) {
   // 1.获取文件内容
-  const source = fs.readFileSync('./example/main.js', {
+  const source = fs.readFileSync(filePath, {
     encoding: 'utf-8'
   })
-  console.log("source", source);
+  console.log("file-source:", source);
   // 2.获取依赖关系
   //  ast => 抽象语法树
   const ast = parser.parse(source, {
@@ -19,7 +20,7 @@ function createAsset() {
   // 获取ast里的路径
   traverse.default(ast,{
     ImportDeclaration({ node }) {
-      console.log("node.source.value", node.source.value);
+      console.log("node.source.value:", node.source.value);
       deps.push(node.source.value)
     }
   })
@@ -32,5 +33,21 @@ function createAsset() {
 
 }
 
-const asset = createAsset()
-console.log("asset", asset);
+// const asset = createAsset()
+// console.log("asset", asset);
+
+// 构建图结构
+function createGraph() {
+  const mainAsset = createAsset('./example/main.js')
+
+  const queue = [mainAsset]
+  for (const asset of queue) {
+    asset.deps.forEach(relativePath => {
+      const child = createAsset(path.resolve('./example', relativePath))
+      console.log('child', child);
+    })
+  }
+
+}
+
+createGraph()

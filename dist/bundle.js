edb7cc5b35755a1188fee28ff60074a859400578
moxunjingmu <1786471868@qq.com>
2022-04-15 00:08:54 +0800

    fix：修改ejs模板

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,40 +1,45 @@
 (function (modules) {
-  function require(filePath) {
-    const fn = modules[filePath]
+  function require(id) {
+    const [fn, mapping] = modules[id]
 
     const module = {
       exports: {}
     }
 
-    fn(require, module, module.exports)
+    function localRequire(filePath) {
+      const id = mapping[filePath]
+      return require(id)
+    }
+
+    fn(localRequire, module, module.exports)
     return module.exports;
   }
-  require('./main.js')
+  require(1)
 })({
+  
+    '0': [function (require, module, exports) {
+      "use strict";
 
-  '0': [function (require, module, exports) {
-    "use strict";
+var _foo = require("./foo.js");
 
-    var _foo = require("./foo.js");
+//不加.js 获取文件时会报错
+(0, _foo.foo)();
+console.log("main");
+    },{"./foo.js":1}],
+  
+  
+    '1': [function (require, module, exports) {
+      "use strict";
 
-    //不加.js 获取文件时会报错
-    (0, _foo.foo)();
-    console.log("main");
-  }, { "./foo.js": 1 }],
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.foo = foo;
 
-
-  '1': [function (require, module, exports) {
-    "use strict";
-
-    Object.defineProperty(exports, "__esModule", {
-      value: true
-    });
-    exports.foo = foo;
-
-    function foo() {
-      console.log("foo.js");
-    }
-  }, {}],
-
-
+function foo() {
+  console.log("foo.js");
+}
+    },{}],
+  
+  
 })

8705c65fc11b387e833fc87788adddd7936dbc30
moxunjingmu <1786471868@qq.com>
2022-04-15 00:10:39 +0800

    fix：require入口改为0

--- dist/bundle.js
+++ dist/bundle.js
@@ -14,32 +14,32 @@
     fn(localRequire, module, module.exports)
     return module.exports;
   }
-  require(1)
+  require(0)
 })({
-  
-    '0': [function (require, module, exports) {
-      "use strict";
 
-var _foo = require("./foo.js");
+  '0': [function (require, module, exports) {
+    "use strict";
 
-//不加.js 获取文件时会报错
-(0, _foo.foo)();
-console.log("main");
-    },{"./foo.js":1}],
-  
-  
-    '1': [function (require, module, exports) {
-      "use strict";
+    var _foo = require("./foo.js");
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.foo = foo;
+    //不加.js 获取文件时会报错
+    (0, _foo.foo)();
+    console.log("main");
+  }, { "./foo.js": 1 }],
 
-function foo() {
-  console.log("foo.js");
-}
-    },{}],
-  
-  
+
+  '1': [function (require, module, exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.foo = foo;
+
+    function foo() {
+      console.log("foo.js");
+    }
+  }, {}],
+
+
 })
